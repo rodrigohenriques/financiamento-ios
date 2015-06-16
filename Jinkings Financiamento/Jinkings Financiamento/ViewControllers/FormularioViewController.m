2cc0ced67404fd72317b815c3594b4b3dee4e869
Guilherme Assis <guilherme.assis@m4u.com.br>
2015-06-16 01:05:09 -0300

    format currency

--- Jinkings Financiamento/Jinkings Financiamento/ViewControllers/FormularioViewController.m
+++ Jinkings Financiamento/Jinkings Financiamento/ViewControllers/FormularioViewController.m
@@ -309,6 +309,65 @@
         }
     }
     
+    if ([textField isEqual:_edtValorFinanciamento] || [textField isEqual:_edtValorImovel]) {
+        NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+        [numberFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
+        [numberFormatter setMaximumFractionDigits:2];
+        [numberFormatter setMinimumFractionDigits:2];
+        
+        NSString *stringMaybeChanged = [NSString stringWithString:string];
+        if (stringMaybeChanged.length > 1)
+        {
+            NSMutableString *stringPasted = [NSMutableString stringWithString:stringMaybeChanged];
+            [stringPasted replaceOccurrencesOfString:numberFormatter.groupingSeparator
+                                          withString:@""
+                                             options:NSLiteralSearch
+                                               range:NSMakeRange(0, [stringPasted length])];
+            
+            NSDecimalNumber *numberPasted = [NSDecimalNumber decimalNumberWithString:stringPasted];
+            stringMaybeChanged = [numberFormatter stringFromNumber:numberPasted];
+        }
+        
+        UITextRange *selectedRange = [textField selectedTextRange];
+        UITextPosition *start = textField.beginningOfDocument;
+        NSInteger cursorOffset = [textField offsetFromPosition:start toPosition:selectedRange.start];
+        NSMutableString *textFieldTextStr = [NSMutableString stringWithString:textField.text];
+        NSUInteger textFieldTextStrLength = textFieldTextStr.length;
+        
+        [textFieldTextStr replaceCharactersInRange:range withString:stringMaybeChanged];
+        
+        [textFieldTextStr replaceOccurrencesOfString:numberFormatter.groupingSeparator
+                                          withString:@""
+                                             options:NSLiteralSearch
+                                               range:NSMakeRange(0, [textFieldTextStr length])];
+        
+        [textFieldTextStr replaceOccurrencesOfString:numberFormatter.decimalSeparator
+                                          withString:@""
+                                             options:NSLiteralSearch
+                                               range:NSMakeRange(0, [textFieldTextStr length])];
+        
+        if (textFieldTextStr.length <= 12)
+        {
+            NSDecimalNumber *textFieldTextNum = [NSDecimalNumber decimalNumberWithString:textFieldTextStr];
+            NSDecimalNumber *divideByNum = [[[NSDecimalNumber alloc] initWithInt:10] decimalNumberByRaisingToPower:numberFormatter.maximumFractionDigits];
+            NSDecimalNumber *textFieldTextNewNum = [textFieldTextNum decimalNumberByDividingBy:divideByNum];
+            NSString *textFieldTextNewStr = [numberFormatter stringFromNumber:textFieldTextNewNum];
+            
+            textField.text = textFieldTextNewStr;
+            
+            if (cursorOffset != textFieldTextStrLength)
+            {
+                NSInteger lengthDelta = textFieldTextNewStr.length - textFieldTextStrLength;
+                NSInteger newCursorOffset = MAX(0, MIN(textFieldTextNewStr.length, cursorOffset + lengthDelta));
+                UITextPosition* newPosition = [textField positionFromPosition:textField.beginningOfDocument offset:newCursorOffset];
+                UITextRange* newRange = [textField textRangeFromPosition:newPosition toPosition:newPosition];
+                [textField setSelectedTextRange:newRange];
+            }
+        }
+        
+        return NO;
+    }
+    
     return YES;
 }
 
