b8768977395b6711f46cf675ebe32f1320c962d8
gui-assis <Guilherme@Mac.local>
2015-06-21 01:52:35 -0300

    Commit

---
+++ Jinkings Financiamento/Pods/Target Support Files/Pods-Jinkings Soluciona-IQKeyboardManager/Pods-Jinkings Soluciona-IQKeyboardManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Jinkings_Soluciona_IQKeyboardManager : NSObject
+@end
+@implementation PodsDummy_Pods_Jinkings_Soluciona_IQKeyboardManager
+@end
---
+++ Jinkings Financiamento/Pods/Target Support Files/Pods-Jinkings Soluciona-TSMessages/Pods-Jinkings Soluciona-TSMessages-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Jinkings_Soluciona_TSMessages : NSObject
+@end
+@implementation PodsDummy_Pods_Jinkings_Soluciona_TSMessages
+@end
